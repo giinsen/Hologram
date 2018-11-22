5e848b45bea00aac197f15d523e33acf19fd223a
giinsen <gabriel.lapegue@gmail.com>
2018-11-22 10:27:21 +0100

    cleaning a little bit

--- Assets/Scripts/Spaceship.cs
+++ Assets/Scripts/Spaceship.cs
@@ -7,13 +7,6 @@ public class Spaceship : MonoBehaviour {
 	void Update ()
     {
         Vector3 v = new Vector3(GetComponentInParent<OrbitorPlayer>().tempInput.x, GetComponentInParent<OrbitorPlayer>().tempInput.y, 0);
-        //Debug.Log(v.normalized);
-
-        Debug.Log("position : " + transform.position + "        " + "rotation" + transform.rotation.eulerAngles + "        " + "v normalized" + v.normalized);
-        //transform.up = transform.position.normalized;
-
-
-        //transform.up = transform.position.normalized;
 
         var pos = ProjectPointOnPlane(transform.position, transform.position, transform.position + v.normalized);
         transform.LookAt(pos, transform.position + v.normalized);
@@ -44,22 +37,6 @@ public class Spaceship : MonoBehaviour {
                 transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x - 180f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z));
             }
         }
-
-
-
-
-        //transform.forward = Vector3.forward;
-        //transform.LookAt(transform.position + v.normalized);
-
-        //transform.right = transform.position.normalized + v.normalized;
-        //transform.up = v.normalized;
-
-        //transform.rotation = Quaternion.Euler(Vector3.Dot(v, transform.position));
-
-        //Quaternion.Euler(transform.rotation.x, transform.rotation.y, 90f);
-        //transform.LookAt(transform.localRotation.eulerAngles + v);
-        //transform.localRotation = Quaternion.LookRotation(new Vector3(GetComponentInParent<OrbitorPlayer>().tempInput.x, GetComponentInParent<OrbitorPlayer>().tempInput.y, 0));
-        //transform.localRotation = Quaternion.Euler(GetComponentInParent<OrbitorPlayer>().tempInput.x, GetComponentInParent<OrbitorPlayer>().tempInput.y, 0);
     }
 
     private Vector3 ProjectPointOnPlane( Vector3 planeNormal, Vector3 planePoint , Vector3 point)
