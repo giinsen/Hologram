966d6883545780a2d55ae1fa909750b132380dca
Ludovic Moge <ludovic.moge@gmail.com>
2018-11-23 13:15:16 +0100

    Sound Manager + audioclips

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameMgr : MonoBehaviour
 {
@@ -23,6 +24,11 @@ public class GameMgr : MonoBehaviour
 		if (isSpawning == false && powerUpInstance == null)
 		{
 			StartCoroutine(SpawnTimer());
+		}
+
+		if (Input.GetKey(KeyCode.R))
+		{
+			SceneManager.LoadScene(0);
 		}
 	}
 
