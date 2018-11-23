8830977ce29e73a247f1f7ae741d1fa6d99a1783
giinsen <gabriel.lapegue@gmail.com>
2018-11-23 13:17:14 +0100

    grid et stars

--- Assets/Marie/Hologramme/Assets/Scripts/Earth.cs
+++ Assets/Marie/Hologramme/Assets/Scripts/Earth.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class Earth : MonoBehaviour
 {
     private float Speed;
+    private float planetScale;
+    public AnimationCurve animationCurveEnter;
 
     private void Start()
     {
         Speed = ParametersMgr.instance.GetParameterFloat("planetRotationSpeed");
-        transform.localScale = Vector3.one * ParametersMgr.instance.GetParameterFloat("planetScale");
+        planetScale = ParametersMgr.instance.GetParameterFloat("planetScale");
+        transform.localScale = Vector3.one * 0f;
     }
 
     void Update()
     {
         this.transform.Rotate(0, Speed * Time.deltaTime, 0);
+        transform.localScale = Vector3.one * planetScale * animationCurveEnter.Evaluate(Time.time);
     }
 }
